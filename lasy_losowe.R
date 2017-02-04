6386aa22132ca59ccc533be725cf493ceb275423
michalcisek <mc56283@sgh.waw.pl>
2017-02-04 16:59:18 +0100

    first attempt using random forest

--- lasy_losowe.R
+++ lasy_losowe.R
@@ -1,2 +1,14 @@
+rm(list=ls())
+source("dane.R")
 library(randomForest)
 
+set.seed(750)
+
+
+rf<-randomForest(factor(Survived) ~ Pclass+Sex+Age+SibSp+Parch+Fare+Embarked,data=train)
+plot(rf)
+
+predykcja<-predict(rf,test)
+
+rf_rozwiazanie<-data.frame(PassengerID=test$PassengerId, Survived=predykcja)
+write.csv(rf_rozwiazanie, file = '20170204_rf_rozwiazanie.csv', row.names = F)
